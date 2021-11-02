7bf100f4d440f7039da417d411b8d33447c219cf
christianxxr <60172861+cdiezmartin@users.noreply.github.com>
2021-11-02 17:43:50 +0100

    actividad3

---
+++ AE-3.Formularios/funciones.js
@@ -0,0 +1,81 @@
+function validacion() {
+    if (nombre.value.trim() == "") {
+        alert('[ERROR] El nombre debe estar relleno')
+        return false
+    }
+
+    if (direccion.value.trim() == "") {
+        alert('[ERROR] La dirección debe estar rellena')
+        return false
+    }
+
+    if (telefono.value.trim() == "") {
+        alert('[ERROR] El teléfono debe estar relleno')
+        return false
+    }
+
+    if (email.value.trim() == "") {
+        alert('[ERROR] El email debe estar relleno')
+        return false
+    }
+
+    tamanio = document.getElementsByName("tamanio")
+    var seleccionadorTamanio = false
+    for (var i = 0; i < tamanio.length; i++) {
+        if (tamanio[i].checked) {
+            seleccionadorTamanio = true
+        }
+    }
+
+    if (!seleccionadorTamanio) {
+        alert('[ERROR] Debe seleccionar el tamaño de la pizza')
+        return false
+    }
+
+    ingredientes = document.getElementsByName("ingredientes")
+    var seleccionadorIngredientes = false
+    for (var i = 0; i < ingredientes.length; i++) {
+        if (ingredientes[i].checked) {
+            seleccionadorIngredientes = true
+        }
+    }
+
+    if (!seleccionadorIngredientes) {
+        alert('[ERROR] Debe seleccionar algún ingrediente de la pizza')
+        return false
+    }
+
+    var comprobarNombrePorMayuscula = /^[A-Z]/
+    esValido = comprobarNombrePorMayuscula.test(nombre.value)
+
+    if (!esValido){
+        alert('[ERROR] El nombre debe empezar por mayúscula')
+        return false;
+    }
+
+    var comprobarTelefono = /^[0-9]{9}$/
+    esValido = comprobarTelefono.test(telefono.value)
+
+    if (!esValido) {
+        alert('[ERROR] Introduzca un teléfono válido')
+        return false;
+    }
+
+
+    var comprobarCorreo = /^\w+([\.-]?\w+)*@(?:|hotmail|outlook|yahoo|live|gmail)\.(?:|com|es)+$/
+    esValido = comprobarCorreo.test(email.value)
+
+    if (!esValido) {
+        alert('[ERROR] Introduzca un correo válido')
+        return false;
+    }
+
+
+    return true
+
+
+}
+
+window.onload = function () {
+    formularioPizzeria.onsubmit = validacion
+}
